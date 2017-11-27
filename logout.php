e3fedbbe9204ba4eaebacda3f9ed136110facd20
kelly-david <davidkelly.tlf@gmail.com>
2017-11-27 14:55:07 +0000

    Change Password

--- logout.php
+++ logout.php
@@ -3,14 +3,13 @@
  * @Author: David Kelly 
  * @Date: 2017-11-24 21:08:35 
  * @Last Modified by: david
- * @Last Modified time: 2017-11-27 14:29:03
+ * @Last Modified time: 2017-11-27 14:52:01
  */
 
 session_start();
 if(isset($_COOKIE[session_name()])) {
     setcookie(session_name(),'',time()-3600); # unset session id/cookies
 }
-unset($_SESSION['username']);
 $_SESSION = array();
 session_destroy();
 session_commit(); 
