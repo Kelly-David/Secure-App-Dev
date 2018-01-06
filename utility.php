a5cf1b3203e83fdc3d5b39894091455083930a85
kelly-david <davidkelly.tlf@gmail.com>
2018-01-06 10:51:10 +0000

    Documentation

--- utility.php
+++ utility.php
@@ -3,9 +3,11 @@
  * @Author: David Kelly 
  * @Date: 2017-11-23 21:14:15 
  * @Last Modified by: david
- * @Last Modified time: 2017-11-24 08:59:42
+ * @Last Modified time: 2018-01-06 10:50:58
  */
 
+
+// Testing fucntion: output to console
 function debug_to_console( $data ) {
     $output = $data;
     if ( is_array( $output ) )
