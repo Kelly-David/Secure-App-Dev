23bfe0c59b229cf3c0af469113db30a3c3736c94
kelly-david <davidkelly.tlf@gmail.com>
2018-01-25 08:53:30 +0000

    Client session on login

--- db/includes/DBMS/MySQL.php
+++ db/includes/DBMS/MySQL.php
@@ -1,5 +1,7 @@
 <?php
 
+include('./utility.php');
+
 /*
 
 This file contains all of the code to setup the initial MySQL database. (setup.php)
@@ -16,31 +18,24 @@ $_DVWA[ 'db_password' ] = '';
 define( 'DVWA_WEB_PAGE_TO_ROOT', '../../../' );
 
 if( !@($GLOBALS["___mysqli_ston"] = mysqli_connect( $_DVWA[ 'db_server' ],  $_DVWA[ 'db_user' ],  $_DVWA[ 'db_password' ] )) ) {
-	dvwaMessagePush( "Could not connect to the MySQL service.<br />Please check the config file." );
-	if ($_DVWA[ 'db_user' ] == "root") {
-		dvwaMessagePush( 'Your database user is root, if you are using MariaDB, this will not work, please read the README.md file.' );
-	}
-	dvwaPageReload();
+	debug_to_console( "Could not connect to the MySQL service.<br />Please check the config file." );
 }
 
 
 // Create database
 $drop_db = "DROP DATABASE IF EXISTS {$_DVWA[ 'db_database' ]};";
 if( !@mysqli_query($GLOBALS["___mysqli_ston"],  $drop_db ) ) {
-	dvwaMessagePush( "Could not drop existing database<br />SQL: " . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) );
-	dvwaPageReload();
+	debug_to_console( "Could not drop existing database<br />SQL: " . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) );
 }
 
 $create_db = "CREATE DATABASE {$_DVWA[ 'db_database' ]};";
 if( !@mysqli_query($GLOBALS["___mysqli_ston"],  $create_db ) ) {
-	dvwaMessagePush( "Could not create database<br />SQL: " . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) );
-	dvwaPageReload();
+	debug_to_console( "Could not create database<br />SQL: " . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) );
 }
 
-// Create table 'users'
+// Create table 'user'
 if( !@((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE " . $_DVWA[ 'db_database' ])) ) {
-	dvwaMessagePush( 'Could not connect to database.' );
-	dvwaPageReload();
+	debug_to_console( 'Could not connect to database.' );
 }
 
 $create_tb = "CREATE TABLE user(
@@ -50,25 +45,31 @@ $create_tb = "CREATE TABLE user(
     lastLogin TIMESTAMP,
     state BOOLEAN NOT NULL DEFAULT 1,
     attempt INT(10) NOT NULL DEFAULT 0,
+	sessionID varchar(33),
     PRIMARY KEY (id)
 );";
 if( !mysqli_query($GLOBALS["___mysqli_ston"],  $create_tb ) ) {
-	dvwaMessagePush( "Table could not be created<br />SQL: " . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) );
-	dvwaPageReload();
+	debug_to_console( "Table could not be created<br />SQL: " . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) );
+}
+
+$create_session_tb = "CREATE TABLE clientSession(
+    SessionID varchar(33) NOT NULL,
+	Counter int(11) NOT NULL,
+	Tstamp datetime NOT NULL,
+    PRIMARY KEY (SessionID)
+);";
+
+if( !mysqli_query($GLOBALS["___mysqli_ston"],  $create_session_tb ) ) {
+	debug_to_console( "Table could not be created<br />SQL: " . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) );
 }
 
 // Insert some data into users
-// Get the base directory for the avatar media...
-$baseUrl  = 'http://' . $_SERVER[ 'SERVER_NAME' ] . $_SERVER[ 'PHP_SELF' ];
-$stripPos = strpos( $baseUrl, 'setup.php' );
-$baseUrl  = substr( $baseUrl, 0, $stripPos ) . 'hackable/users/';
 
 $insert = "INSERT INTO 
 	user (username, passcode) 
 		VALUES ('admin','Password123');";
 if( !mysqli_query($GLOBALS["___mysqli_ston"],  $insert ) ) {
-	dvwaMessagePush( "Data could not be inserted into 'users' table<br />SQL: " . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) );
-	dvwaPageReload();
+	debug_to_console( "Data could not be inserted into 'users' table<br />SQL: " . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) );
 }
 
 // Done
